d43202cbda15ae30a903939b2ef212c72e51f480
ttesla <omer.akyol@gmail.com>
2022-04-01 19:02:21 +0300

    second update
    update

--- Raytrace_1/Raytrace_1/src/Main.cpp
+++ Raytrace_1/Raytrace_1/src/Main.cpp
@@ -1,15 +1,55 @@
 #include <iostream>
 #include <fstream>
 
-#include "color.h"
-#include "vec3.h"
+#include "Color.h"
+#include "Vec3.h"
+#include "Ray.h"
 
 using namespace std;
 
-void WriteToFile()
+double hit_sphere(const point3& center, double radius, const ray& r) 
 {
-    const int image_width = 256;
-    const int image_height = 256;
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-half_b - sqrt(discriminant)) / a;
+    }
+}
+
+color ray_color(const ray& r) 
+{
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+void Render()
+{
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     ofstream outputFile;
     outputFile.open("render.ppm");
@@ -21,7 +61,10 @@ void WriteToFile()
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) 
         {
-            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
             write_color(outputFile, pixel_color);
         }
     }
@@ -32,6 +75,6 @@ void WriteToFile()
 
 int main()
 {
-    WriteToFile();
+    Render();
     return 0;
 }
