e7c0ac5c2ca7f99628720bf8b90fec4074379a0e
ttesla <omer.akyol@gmail.com>
2022-04-04 19:03:43 +0300

    first render
    first render

--- Raytrace_1/Raytrace_1/src/Main.cpp
+++ Raytrace_1/Raytrace_1/src/Main.cpp
@@ -1,11 +1,17 @@
+#include "Definitions.h"
+#include "Ray.h"
+#include "HittableList.h"
+#include "Sphere.h"
+#include "Color.h"
+#include "Camera.h"
+
 #include <iostream>
 #include <fstream>
 
-#include "Color.h"
-#include "Vec3.h"
-#include "Ray.h"
-
-using namespace std;
+using std::endl;
+using std::cout;
+using std::cerr;
+using std::ofstream;
 
 double hit_sphere(const point3& center, double radius, const ray& r) 
 {
@@ -23,15 +29,22 @@ double hit_sphere(const point3& center, double radius, const ray& r)
     }
 }
 
-color ray_color(const ray& r) 
+color ray_color(const ray& r, const hittable& world, int depth)
 {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    hit_record rec;
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0, 0, 0);
+    
+    if (world.hit(r, 0.001, Infinity, rec)) 
+    {
+        point3 target = rec.p + rec.normal + random_unit_vector();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
+
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -41,16 +54,18 @@ void Render()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-    auto origin = point3(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    // Camera
+    camera cam;
 
+    // Write PPM File
     ofstream outputFile;
     outputFile.open("render.ppm");
 
@@ -58,19 +73,26 @@ void Render()
 
     for (int j = image_height - 1; j >= 0; --j) 
     {
-        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        cout << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) 
         {
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            color pixel_color = ray_color(r);
-            write_color(outputFile, pixel_color);
+            color pixel_color(0, 0, 0);
+
+            // Sampling
+            for (int s = 0; s < samples_per_pixel; ++s) 
+            {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_color += ray_color(r, world, max_depth);
+            }
+            
+            write_color(outputFile, pixel_color, samples_per_pixel);
         }
     }
 
     outputFile.close();
-    std::cerr << "\nDone.\n";
+    cout << "\nDone.\n";
 }
 
 int main()
