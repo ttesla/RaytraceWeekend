77b13821e741883cf261b4b7eb759583efe5e670
ttesla <omer.akyol@gmail.com>
2022-04-07 16:21:26 +0300

    delete ppm file

--- Raytrace_1/Raytrace_1/src/Main.cpp
+++ Raytrace_1/Raytrace_1/src/Main.cpp
@@ -61,21 +61,78 @@ color ray_color(const ray& r, const hittable& world, int depth)
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
+hittable_list random_scene() 
+{
+    hittable_list world;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
+    int balls = 11;
+
+    for (int a = -balls; a < balls; a++) 
+    {
+        for (int b = -balls; b < balls; b++) 
+        {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9)
+            {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) 
+                {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else if (choose_mat < 0.95) 
+                {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+                else 
+                {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    world.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    world.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.8, 0.8, 0.8), 0.0);
+    world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    return world;
+}
+
 void Render()
 {
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 600;
+    const int image_width = 1280;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 50;
+    const int samples_per_pixel = 500;
     const int max_depth = 50;
 
     // World
-    hittable_list world;
+    auto world = random_scene();
     /*world.add(make_shared<sphere>(point3(0, 0, -1), 0.5, nullptr));
     world.add(make_shared<sphere>(point3(0, -100.5, -1), 100, nullptr));*/
 
-    auto material_ground = make_shared<lambertian>(color(0.3, 0.3, 0.3));
+    /*auto material_ground = make_shared<lambertian>(color(0.3, 0.3, 0.3));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
     auto material_blue   = make_shared<lambertian>(color(0.1, 0.1, 0.8));
     auto material_green   = make_shared<lambertian>(color(0.1, 0.8, 0.1));
@@ -93,11 +150,17 @@ void Render()
     world.add(make_shared<sphere>(point3(0.5, -0.25, 0.0), 0.25, material_glass_2));
     world.add(make_shared<sphere>(point3(0.5, 1.0, -1.0), 0.5, material_blue));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -2.0), 0.5, material_green));
-    world.add(make_shared<sphere>(point3(-2.0, 0.0, -1.0), 0.5, material_glass_2));
+    world.add(make_shared<sphere>(point3(-2.0, 0.0, -1.0), 0.5, material_glass_2));*/
 
 
     // Camera
-    camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 60, aspect_ratio);
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    vec3 vup(0, 1, 0);
+    auto dist_to_focus = 10.0;
+    auto aperture = 0.1;
+    auto vFov = 20;
+    camera cam(lookfrom, lookat, vup, vFov, aspect_ratio, aperture, dist_to_focus);
 
     // Write PPM File
     ofstream outputFile;
